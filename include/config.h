1252d55e96d671ba6d1ef53d445bc117eee6f5cb
shuge <127205012+haohaoxinsheng@users.noreply.github.com>
2026-05-13 15:17:57 +0800

    tweak: shorten night light hold time

--- include/config.h
+++ include/config.h
@@ -13,5 +13,5 @@ constexpr uint32_t FALL_VIBRATION_WINDOW_MS = 90UL * 1000UL;
 constexpr uint32_t ALERT_BEEP_INTERVAL_MS = 350;
 constexpr uint32_t WARNING_BEEP_INTERVAL_MS = 650;
 constexpr uint32_t CRITICAL_BLINK_INTERVAL_MS = 220;
-constexpr uint32_t NIGHT_LIGHT_HOLD_MS = 90UL * 1000UL;
+constexpr uint32_t NIGHT_LIGHT_HOLD_MS = 5UL * 1000UL;
 }  // namespace Timing
